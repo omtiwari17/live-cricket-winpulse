b831d3a2be84d6883624fdb965a003f84959a607
Om Tiwari <work.om.tiwari@gmail.com>
2026-03-31 16:20:51 +0530

    fix: whitenoise for static files in production

--- winpulse/settings.py
+++ winpulse/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,3 +78,5 @@ CRICKET_API_KEYS = [
 CRICKET_API_KEYS = [key for key in CRICKET_API_KEYS if key]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
