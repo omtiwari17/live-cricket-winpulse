d620176c73e209d206e3435a57e9184ca9f77c62
Om Tiwari <work.om.tiwari@gmail.com>
2026-04-03 00:26:21 +0530

    automate superuser creation in build script

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py createsuperuser --noinput --username $ADMIN_USERNAME --email $ADMIN_EMAIL || true
